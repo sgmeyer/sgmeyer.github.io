b1029c879e78c200c70f18e63e90e66c9384bc26
Shawn Meyer <sgmeyer@gmail.com>
2016-06-11 22:14:49 -0500

    Beginning to add project look up by language.

--- _site/scripts/app/repositoryService.js
+++ _site/scripts/app/repositoryService.js
@@ -1,25 +1,52 @@
-(function(window, nanoajax, undefined) {
+(function(window, nanoajax, JSON, undefined) {
   'use strict';
 
+  var repositoriesUrl = 'https://api.github.com/users/sgmeyer/repos';
   var repositoryService = window.repositoryService || {};
 
-  repositoryService.getLanguages = function(callback) {
-    return nanoajax.ajax({url:'https://api.github.com/users/sgmeyer/repos'}, function (code, responseText) {
+  var getLanguages = function(success, error) {
+    return nanoajax.ajax({url: repositoriesUrl}, function (code, responseText) {
       var languages = new Set();
       var responseObject = {};
-      if(code === 200) {
+      if(code === 200 && success) {
         responseObject = JSON.parse(responseText);
-
         responseObject.forEach(function (element, index, array) {
           var language = element.language;
           if (language) {
             languages.add(language);
           }
         });
+
+        success(Array.from(languages));
+      } else if (error) {
+        error('Failed to get languages.');
       }
-      callback(code, Array.from(languages));
     });
   };
 
+  var getProjects = function(language, success, error) {
+    return nanoajax.ajax({url: repositoriesUrl}, function (code, responseText) {
+      var projects = [];
+      var responseObject = {};
+      if (code == 200 && success) {
+        responseObject = JSON.parse(responseText);
+        responseObject.forEach(function (element, index, array) {
+          if (element.language === language) {
+            projects.push({
+              name: element.name,
+              description: element.description
+            });
+          }
+        });
+
+        success(projects);
+      } else if (error) {
+        error('Failed to get projects.');
+      }
+    });
+  };
+
+  repositoryService.getLanguages = getLanguages;
+  repositoryService.getProjects = getProjects;
   window.repositoryService = repositoryService;
-})(window, nanoajax);
+})(window, nanoajax, JSON);
--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,20 +1,42 @@
-(function () {
+(function (repositoryService) {
   'use strict';
 
-  var listLanguages = function (code, languages) {
+  var listProjects = function (projects) {
+    projects.forEach(function (element, index, array) {
+      console.log(element);
+    });
+  };
+
+  /**
+   * Constructs a language li for project-languages navigation.
+   **/
+  var listLanguages = function (languages) {
     var elm = document.getElementById('project-languages');
     languages.forEach(function (element, index, array) {
       var li = document.createElement('li');
       var a = document.createElement('a');
+      a.setAttribute('href', '#');
+      a.setAttribute('data-project-language', element);
+      a.onclick = languageOnClick;
       var txt = document.createTextNode(element);
 
       a.appendChild(txt);
       li.appendChild(a);
       elm.appendChild(li);
     });
+
+    initializeFirstLanguage(languages[0]);
   }
 
-  var x = window.repositoryService.getLanguages(listLanguages)
-  console.log(x);
+  var languageOnClick = function() {
+    var language = this.getAttribute('data-project-language');
+    window.repositoryService.getProjects(language, listProjects);
+  };
 
-})();
+  var initializeFirstLanguage = function (language) {
+    window.repositoryService.getProjects(language, listProjects);
+  }
+
+  window.repositoryService.getLanguages(listLanguages);
+
+})(repositoryService);
